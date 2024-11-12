c5108033d7703c6bdf358fd6127c61d23dad2268
Mathéo <matheogirard@hotmail.com>
2024-11-12 16:46:54 +0100

    feat: ajout des fonctionnalités de gestion des spectacles et des réservations
    - Ajout des fonctions pour ajouter, supprimer et modifier des spectacles
    - Implémentation de la gestion des réservations avec ajout, annulation et modification
    - Amélioration de la fonction trouver_alternative pour proposer des alternatives

---
+++ build/CMakeFiles/client.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/client.dir/Sources/ipc_utils.c.o"
+  "CMakeFiles/client.dir/Sources/ipc_utils.c.o.d"
+  "CMakeFiles/client.dir/Sources/spectacles.c.o"
+  "CMakeFiles/client.dir/Sources/spectacles.c.o.d"
+  "CMakeFiles/client.dir/client.c.o"
+  "CMakeFiles/client.dir/client.c.o.d"
+  "client"
+  "client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/serveur.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/serveur.dir/Sources/ipc_utils.c.o"
+  "CMakeFiles/serveur.dir/Sources/ipc_utils.c.o.d"
+  "CMakeFiles/serveur.dir/Sources/main.c.o"
+  "CMakeFiles/serveur.dir/Sources/main.c.o.d"
+  "CMakeFiles/serveur.dir/Sources/spectacles.c.o"
+  "CMakeFiles/serveur.dir/Sources/spectacles.c.o.d"
+  "serveur"
+  "serveur.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/serveur.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
